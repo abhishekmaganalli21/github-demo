3506ed86acc19854d2bbfce05b0cec9eacbbb3ac
abhishekmaganalli21 <86823380+abhishekmaganalli21@users.noreply.github.com>
2021-07-02 14:20:57 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#Add implementation
+def add(x,y):
+    return x+y
+#Subtract inplementation
+def subtract(x,y):
+    return x-y          #on bug456 branch
+#Multiply inplementation
+def multiply(x,y):
+    return x*y      #on master branch
+#Divide inplementation
+def divide(x,y):
+    if y==0:        #on bug789 branch
+        return error
+       else:
+        return x/y
